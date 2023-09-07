ca8c641f800796a827893589ac81ae8ae61d066f
Kim <cestbonciel88@knou.ac.kr>
2023-09-08 02:13:30 +0900

    Add: figure out the difference between array and the length of array

--- Programmers/Programmers/PS basic/testArray.swift
+++ Programmers/Programmers/PS basic/testArray.swift
@@ -6,12 +6,13 @@
 //
 
 import Foundation
-
+//MARK: - Array 의 Element 값으로 접근
 let num_list = [5, 2, 1, 7, 5]
 for i in num_list {
 	print("index: \(i)")
 }
 print("divider:----")
+//MARK: - Index순서 로 접근
 for j in 0...num_list.count {
 	print("index2: \(j)")
 }
