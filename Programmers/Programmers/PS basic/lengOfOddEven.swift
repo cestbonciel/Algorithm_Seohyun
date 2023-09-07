107e7b415aa8a095112db5c33712b59c549417be
Kim <cestbonciel88@knou.ac.kr>
2023-09-08 02:19:12 +0900

    Modify: 짝수 홀수 개수 제한사항 2번째 추가

--- Programmers/Programmers/PS basic/lengOfOddEven.swift
+++ Programmers/Programmers/PS basic/lengOfOddEven.swift
@@ -9,15 +9,19 @@ import Foundation
 
 //MARK: 짝수 홀수 개수
 func solution(_ num_list: [Int]) -> [Int] {
+	//MARK: - 제한사항1: num_list 길이
 	guard num_list.count >= 1 && num_list.count <= 100 else { return [] }
 	var oddNumSequence: [Int] = []
 	var evenNumSequence: [Int] = []
 	var composition: [Int] = []
 	for i in num_list {
-		if i % 2 == 1 {
-			oddNumSequence.append(i)
-		} else {
-			evenNumSequence.append(i)
+		// MARK: - 제한사항2: num_list의 원소
+		if i >= 0 && i <= 1000 {
+			if i % 2 == 1 {
+				oddNumSequence.append(i)
+			} else {
+				evenNumSequence.append(i)
+			}
 		}
 	}
 	composition.insert(evenNumSequence.count, at: 0)
